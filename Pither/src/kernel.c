2690bf6fb16066edf54462ec6ccf52d23d472096
Haney, Joshua <joshua.haney9987@gmail.com>
2025-04-12 13:49:37 -0500

    debugging

--- Pither/src/kernel.c
+++ Pither/src/kernel.c
@@ -2,13 +2,6 @@
 
 #define UART0_DR ((volatile uint32_t*)(0x20201000)) // Pi specific for memory
 
-void uart_puts(const char *str) {
-	while (*str) {
-		*UART0_DR = (uint32_t)(*str++);
-
-	}
-}
-
 void kernel_main(void) {   // This is the main function for my kernel. Its like main()
 	*UART0_DR = 'X';
 	while (1); // halt
