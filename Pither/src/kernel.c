0b9a135b8a8798dd1285bd585bfc667174d4b100
Haney, Joshua <joshua.haney9987@gmail.com>
2025-04-12 12:32:32 -0500

    Adding files after make

--- Pither/src/kernel.c
+++ Pither/src/kernel.c
@@ -1,6 +1,6 @@
 #include <stdint.h> // Includes definitions for fixed-width integer types (such as uint32_t). Useful in embedded development where memory and layout must be precise
 
-#define UART0_DR ((volatile uint32_t*)(0x020201000)) // Pi specific for memory
+#define UART0_DR ((volatile uint32_t*)(0x20201000)) // Pi specific for memory
 
 void uart_puts(const char *str) {
 	while (*str) {
@@ -10,6 +10,6 @@ void uart_puts(const char *str) {
 }
 
 void kernel_main(void) {   // This is the main function for my kernel. Its like main()
-	uart_puts("Welcome to NeuroPi\n");
+	uart_puts("Welcome to Pither\n");
 	while (1); // halt
 }
