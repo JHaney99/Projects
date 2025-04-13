2bc6cef710b9d911312caa8700159fb8fb92fc25
Haney, Joshua <joshua.haney9987@gmail.com>
2025-04-12 20:54:42 -0500

    adding comments

--- Pither/include/mmio.h
+++ Pither/include/mmio.h
@@ -3,12 +3,12 @@
 
 #include <stdint.h>
 
-static inline void mmio_write(uint32_t reg, uint32_t data) {
-	*(volatile uint32_t*)reg = data;
+static inline void mmio_write(uint32_t reg, uint32_t data) { // uint32_t - unsigned 32 bits ---Width of each register. If dealing with 64 bit registers, would probably need to use 64. Data going to registers will also be the size of the register (duh)
+	*(volatile uint32_t*)reg = data; // Setting a register equal to the value of data. (volatile uint32_t*)reg points to the register (reg) *(volatile uint32_t*)reg = data goes to the actualy memory location and stores the value data in there
 }
 
 static inline uint32_t mmio_read(uint32_t reg) {
-	return *(volatile uint32_t*)reg;
+	return *(volatile uint32_t*)reg; // Returns the value of whatever is in the register. If I just did (volatile uint32_t*)reg it would return the memory location itself
 }
 
 #endif
--- Pither/src/uart.c
+++ Pither/src/uart.c
@@ -1,7 +1,7 @@
 #include "mmio.h"
 #include "uart.h"
 
-#define UART0_BASE 0x101f1000 // For QEMU versatilepb
+#define UART0_BASE 0x101f1000 // For QEMU versatilepb WHERE UART PINS/MEMORY WOULD BE
 #define UART0_DR	(UART0_BASE + 0x00)
 
 void uart_init(void) {
@@ -9,11 +9,11 @@ void uart_init(void) {
 }
 
 void uart_write_char(char c){
-	mmio_write(UART0_DR, (uint32_t)c);
+	mmio_write(UART0_DR, (uint32_t)c); //Write a character to a register?
 }
 
 void uart_write_string(const char* str) {
 	while (*str) {
-		uart_write_char(*str++);
+		uart_write_char(*str++); //Iterate through the string and write character to location using mmio_write
 	}
 }
