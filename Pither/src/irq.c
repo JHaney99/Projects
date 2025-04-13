b2331d6a79ad953e40ac98d98f1a4c899f7e86b6
Haney, Joshua <joshua.haney9987@gmail.com>
2025-04-12 23:25:24 -0500

    Adding iterrupt controller

---
+++ Pither/src/irq.c
@@ -0,0 +1,18 @@
+#include "irq.h"
+#include "mmio.h"
+
+#define VIC_IRQSTATUS (*(volatile uint32_t*)0x10140000)
+#define VIC_SOFTINT (*(volatile uint32_t*)0x10140008)
+#define VIC_INTENABLE (*(volatile uint32_t*)0x10140010)
+
+void c_irq_handler(void) {
+    uint32_t status = VIC_IRQSTATUS;
+
+    if (status & (1 << 3)) {
+        // IRQ from software interrupt channel 3
+        uart_write_string("[IRQ] Software interrupt received\n");
+
+        // Clear the software interrupt
+        VIC_SOFTINT &= ~(1 << 3);
+    }
+}
--- Pither/src/kernel.c
+++ Pither/src/kernel.c
@@ -1,8 +1,22 @@
 #include "uart.h"
+#include "irq.h"
+#include <stdint.h>
+
+#define VIC_INTENABLE (*(volatile uint32_t*)0x10140010)
+#define VIC_SOFTINT (*(volatile uint32_t*)0x10140008)
+
 
 void kernel_main(void) {   // This is the main function for my kernel. Its like main()
 	uart_init();
 	uart_write_string("Welcome to Pither via UART HAL\n");
+
+	uart_write_string("IRQ testing\n");
+
+	// Enable software interrupt bit
+	VIC_INTENABLE = (1 << 3);
+	
+	// Trigger the interrupt
+	VIC_SOFTINT = (1 << 3);
 
 	while(1);
 }
